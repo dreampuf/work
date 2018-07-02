5fcd09121641e6f17971f97213744869931681b6
Xin Huang <xhuang@freewheel.tv>
2018-07-02 14:55:17 -0400

    Update prefix

--- webui/internal/assets/src/index.js
+++ webui/internal/assets/src/index.js
@@ -46,7 +46,7 @@ class App extends React.Component {
 // See https://github.com/reactjs/react-router/issues/1857.
 render(
   <Router history={hashHistory}>
-    <Route path="./" component={App}>
+    <Route path={this.props.prefix} component={App}>
       <Route path="/processes" component={ () => <Processes busyWorkerURL="/busy_workers" workerPoolURL="/worker_pools" /> } />
       <Route path="/queues" component={ () => <Queues url="/queues" /> } />
       <Route path="/retry_jobs" component={ () => <RetryJobs url="/retry_jobs" /> } />
